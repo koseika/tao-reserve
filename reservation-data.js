40f408495441b9921d481a2496b7f1146d4c167c
koseika <hro.c.222@gmail.com>
2026-06-26 16:23:26 +0900

    Create reservation-data.js

---
+++ reservation-data.js
@@ -0,0 +1,35 @@
+// Tao Reserve 予約データ
+
+// status: "available" = ○
+
+// status: "unavailable" = ×
+
+// times: 表示する開始時間
+
+const reservationData = {
+
+  "2026-06-24": {
+
+    status: "available",
+
+    times: ["15:00", "16:00", "17:00", "18:00", "19:00"]
+
+  },
+
+  "2026-06-25": {
+
+    status: "available",
+
+    times: ["17:00", "18:00", "19:00"]
+
+  },
+
+  "2026-06-26": {
+
+    status: "unavailable",
+
+    times: []
+
+  }
+
+};
